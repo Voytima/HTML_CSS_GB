693950f8a9e827545ffffe81166f26d4f7c5a76b
Dmitry Voytik <93672828+Voytima@users.noreply.github.com>
2023-04-28 23:00:51 +0200

    Delete sem10 directory

--- sem10/script.js
+++
@@ -1,8 +0,0 @@
-const menuActive = document.querySelector('.menu-active');
-const headerMenu = document.querySelector('.user-list__item-menu');
-
-function activator() {
-	menuActive.classList.toggle('hidden___menu'); 
-}
-
-headerMenu.addEventListener('click', activator); 
